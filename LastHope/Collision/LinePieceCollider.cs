0dc0f7fa72cdd9d078b4013526365351ff8d96ef
Stéphane Dauvillier <dauvillier04@gmail.com>
2026-03-19 16:04:07 +0100

    Fixed some stuff

--- LastHope/Collision/LinePieceCollider.cs
+++ LastHope/Collision/LinePieceCollider.cs
@@ -1,274 +1,269 @@
 using System;
-using LastHope.Collision;
 using Microsoft.Xna.Framework;
 
+namespace LastHope.Collision;
 
-
-namespace LastHope.Collision
+public class LinePieceCollider : Collider, IEquatable<LinePieceCollider>
 {
 
-    // public class LinePieceCollider : Collider, IEquatable<LinePieceCollider>
-    // {
+    public Vector2 Start;
+    public Vector2 End;
 
-    //     public Vector2 Start;
-    //     public Vector2 End;
+    /// <summary>
+    /// The length of the LinePiece, changing the length moves the end vector to adjust the length.
+    /// </summary>
+    public float Length
+    {
+        get
+        {
+            return (End - Start).Length();
+        }
+        set
+        {
+            End = Start + GetDirection() * value;
+        }
+    }
 
-    //     /// <summary>
-    //     /// The length of the LinePiece, changing the length moves the end vector to adjust the length.
-    //     /// </summary>
-    //     public float Length
-    //     {
-    //         get
-    //         {
-    //             return (End - Start).Length();
-    //         }
-    //         set
-    //         {
-    //             End = Start + GetDirection() * value;
-    //         }
-    //     }
+    /// <summary>
+    /// The A component from the standard line formula Ax + By + C = 0
+    /// </summary>
+    public float StandardA
+    {
+        // A = Py - Qy
+        get
+        {
+            var a = Start.Y - End.Y;
+            return a;
+        }
+    }
 
-    //     /// <summary>
-    //     /// The A component from the standard line formula Ax + By + C = 0
-    //     /// </summary>
-    //     public float StandardA
-    //     {
-    //         // A = Py - Qy
-    //         get
-    //         {
-    //             var a = Start.Y - End.Y;
-    //             return a;
-    //         }
-    //     }
+    /// <summary>
+    /// The B component from the standard line formula Ax + By + C = 0
+    /// </summary>
+    public float StandardB
+    {
+        // B = Qx - Px
+        get
+        {
+            var b = End.X - Start.X;
+            return b;
+        }
+    }
 
-    //     /// <summary>
-    //     /// The B component from the standard line formula Ax + By + C = 0
-    //     /// </summary>
-    //     public float StandardB
-    //     {
-    //         // B = Qx - Px
-    //         get
-    //         {
-    //             var b = End.X - Start.X;
-    //             return b;
-    //         }
-    //     }
+    /// <summary>
+    /// The C component from the standard line formula Ax + By + C = 0
+    /// </summary>
+    public float StandardC
+    {
+        // C = Px X Qy - Py X Qx
+        get
+        {
+            var c = Start.X * End.Y - Start.Y * End.X;
+            return c;
+        }
+    }
 
-    //     /// <summary>
-    //     /// The C component from the standard line formula Ax + By + C = 0
-    //     /// </summary>
-    //     public float StandardC
-    //     {
-    //         // C = Px X Qy - Py X Qx
-    //         get
-    //         {
-    //             var c = Start.X * End.Y - Start.Y * End.X;
-    //             return c;
-    //         }
-    //     }
+    public LinePieceCollider(Vector2 start, Vector2 end)
+    {
+        Start = start;
+        End = end;
+    }
 
-    //     public LinePieceCollider(Vector2 start, Vector2 end)
-    //     {
-    //         Start = start;
-    //         End = end;
-    //     }
+    public LinePieceCollider(Vector2 start, Vector2 direction, float length)
+    {
+        Start = start;
+        End = start + direction * length;
+    }
 
-    //     public LinePieceCollider(Vector2 start, Vector2 direction, float length)
-    //     {
-    //         Start = start;
-    //         End = start + direction * length;
-    //     }
-
-    //     /// <summary>
-    //     /// Should return the angle between a given direction and the up vector.
-    //     /// </summary>
-    //     /// <param name="direction">The Vector2 pointing out from (0,0) to calculate the angle to.</param>
-    //     /// <returns> The angle in radians between the the up vector and the direction to the cursor.</returns>
-    //     public static float GetAngle(Vector2 direction)
-    //     {
-    //         var angle = (float)Math.Atan2(direction.X, -(direction.Y));
-    //         return angle;
-    //     }
+    /// <summary>
+    /// Should return the angle between a given direction and the up vector.
+    /// </summary>
+    /// <param name="direction">The Vector2 pointing out from (0,0) to calculate the angle to.</param>
+    /// <returns> The angle in radians between the the up vector and the direction to the cursor.</returns>
+    public static float GetAngle(Vector2 direction)
+    {
+        var angle = (float)Math.Atan2(direction.X, -(direction.Y));
+        return angle;
+    }
 
 
-    //     /// <summary>
-    //     /// Calculates the normalized vector pointing from point1 to point2
-    //     /// </summary>
-    //     /// <returns> A Vector2 containing the direction from point1 to point2. </returns>
-    //     public static Vector2 GetDirection(Vector2 point1, Vector2 point2)
-    //     {
-    //         var direction = point2 - point1;
-    //         var normalizedDirection = Vector2.Normalize(direction);
-    //         return normalizedDirection;
-    //     }
+    /// <summary>
+    /// Calculates the normalized vector pointing from point1 to point2
+    /// </summary>
+    /// <returns> A Vector2 containing the direction from point1 to point2. </returns>
+    public static Vector2 GetDirection(Vector2 point1, Vector2 point2)
+    {
+        var direction = point2 - point1;
+        var normalizedDirection = Vector2.Normalize(direction);
+        return normalizedDirection;
+    }
 
 
-    //     /// <summary>
-    //     /// Gets whether or not the Line intersects another Line
-    //     /// </summary>
-    //     /// <param name="other">The Line to check for intersection</param>
-    //     /// <returns>true there is any overlap between the line and the Line.</returns>
-    //     public override bool Intersects(LinePieceCollider other)
-    //     {
-    //         //https://stackoverflow.com/questions/4543506/algorithm-for-intersection-of-2-lines
-    //         var intersectPoint = GetIntersection(other);
-    //         float delta = this.StandardA * other.StandardB - other.StandardA * this.StandardB;
-    //         if (delta == 0)
-    //         {
-    //             return false;
-    //         }
+    /// <summary>
+    /// Gets whether or not the Line intersects another Line
+    /// </summary>
+    /// <param name="other">The Line to check for intersection</param>
+    /// <returns>true there is any overlap between the line and the Line.</returns>
+    public override bool Intersects(LinePieceCollider other)
+    {
+        //https://stackoverflow.com/questions/4543506/algorithm-for-intersection-of-2-lines
+        var intersectPoint = GetIntersection(other);
+        float delta = this.StandardA * other.StandardB - other.StandardA * this.StandardB;
+        if (delta == 0)
+        {
+            return false;
+        }
 
-    //         if (this.Contains(intersectPoint) && other.Contains(intersectPoint))
-    //         {
-    //             return true;
-    //         }
-    //         return false;
-    //     }
-
-
-    //     /// <summary>
-    //     /// Gets whether or not the line intersects a Circle.
-    //     /// </summary>
-    //     /// <param name="other">The Circle to check for intersection.</param>
-    //     /// <returns>true there is any overlap between the two Circles.</returns>
-    //     public override bool Intersects(CircleCollider other)
-    //     {
-    //         var nearestPoint = NearestPointOnLine(other.Center);
-    //         if (other.Contains(nearestPoint))
-    //         {
-    //             return true;
-    //         }
-    //         return false;
-    //     }
-
-    //     /// <summary>
-    //     /// Gets whether or not the Line intersects the Rectangle.
-    //     /// </summary>
-    //     /// <param name="other">The Rectangle to check for intersection.</param>
-    //     /// <returns>true there is any overlap between the Line and the Rectangle.</returns>
-    //     public override bool Intersects(RectangleCollider other)
-    //     {
-    //         var A = new Vector2(other.shape.Left, other.shape.Top);
-    //         var B = new Vector2(other.shape.Right, other.shape.Top);
-    //         var C = new Vector2(other.shape.Right, other.shape.Bottom);
-    //         var D = new Vector2(other.shape.Left, other.shape.Bottom);
-
-    //         var AB = new LinePieceCollider(A, B);
-    //         var AD = new LinePieceCollider(A, D);
-    //         var BC = new LinePieceCollider(B, C);
-    //         var CD = new LinePieceCollider(C, D);
-
-    //         if (this.Intersects(AB) || this.Intersects(AD) || this.Intersects(BC) || this.Intersects(CD) || other.Contains(Start) || other.Contains(End))
-    //         {
-    //             return true;
-    //         }
-    //         return false;
-    //     }
-
-    //     /// <summary>
-    //     /// Calculates the intersection point between 2 lines.
-    //     /// </summary>
-    //     /// <param name="Other">The line to intersect with</param>
-    //     /// <returns>A Vector2 with the point of intersection.</returns>
-    //     public Vector2 GetIntersection(LinePieceCollider Other)
-    //     {
-    //         float delta = this.StandardA * Other.StandardB - Other.StandardA * this.StandardB;
-    //         if (delta == 0)
-    //         {
-    //             return Vector2.Zero;
-    //         }
-    //         float x = (Other.StandardB * this.StandardC - this.StandardB * Other.StandardC) / delta;
-    //         float y = (this.StandardA * Other.StandardC - Other.StandardA * this.StandardC) / delta;
-    //         Vector2 result = new Vector2(x, y);
-    //         return result;
-    //     }
-
-    //     /// <summary>
-    //     /// Finds the nearest point on a line to a given vector, taking into account if the line is .
-    //     /// </summary>
-    //     /// <param name="other">The Vector you want to find the nearest point to.</param>
-    //     /// <returns>The nearest point on the line.</returns>
-    //     public Vector2 NearestPointOnLine(Vector2 other)
-    //     {
-    //         //https://stackoverflow.com/questions/3120357/get-closest-point-to-a-line
-    //         var AP = other - Start;
-    //         var AB = End - Start;
-
-    //         var magnitudeAB = AB.LengthSquared();
-    //         var ABAPproduct = Vector2.Dot(AP, AB);
-    //         var distance = ABAPproduct / magnitudeAB;
-    //         if (distance < 0)
-    //         {
-    //             return Start;
-    //         }
-    //         else if (distance > 1)
-    //         {
-    //             return End;
-    //         }
-    //         else
-    //         {
-    //             return Start + AB * distance;
-    //         }
-    //     }
-
-    //     /// <summary>
-    //     /// Returns the enclosing Axis Aligned Bounding Box containing the control points for the line.
-    //     /// As an unbound line has infinite length, the returned bounding box assumes the line to be bound.
-    //     /// </summary>
-    //     /// <returns></returns>
-    //     public override Rectangle GetBoundingBox()
-    //     {
-    //         Point topLeft = new Point((int)Math.Min(Start.X, End.X), (int)Math.Min(Start.Y, End.Y));
-    //         Point size = new Point((int)Math.Max(Start.X, End.X), (int)Math.Max(Start.Y, End.Y)) - topLeft;
-    //         return new Rectangle(topLeft, size);
-    //     }
+        if (this.Contains(intersectPoint) && other.Contains(intersectPoint))
+        {
+            return true;
+        }
+        return false;
+    }
 
 
-    //     /// <summary>
-    //     /// Gets whether or not the provided coordinates lie on the line.
-    //     /// </summary>
-    //     /// <param name="coordinates">The coordinates to check.</param>
-    //     /// <returns>true if the coordinates are on the line.</returns>
-    //     public override bool Contains(Vector2 coordinates)
-    //     {
-    //         var onTheLine = (coordinates.X >= this.Start.X && coordinates.X <= this.End.X || coordinates.X >= this.End.X && coordinates.X <= this.Start.X)
-    //                         && (coordinates.Y >= this.Start.Y && coordinates.Y <= this.End.Y || coordinates.Y >= this.End.Y && coordinates.Y <= this.Start.Y);
-    //         return onTheLine;
-    //     }
+    /// <summary>
+    /// Gets whether or not the line intersects a Circle.
+    /// </summary>
+    /// <param name="other">The Circle to check for intersection.</param>
+    /// <returns>true there is any overlap between the two Circles.</returns>
+    public override bool Intersects(CircleCollider other)
+    {
+        var nearestPoint = NearestPointOnLine(other.Center);
+        if (other.Contains(nearestPoint))
+        {
+            return true;
+        }
+        return false;
+    }
 
-    //     public bool Equals(LinePieceCollider other)
-    //     {
-    //         return other.Start == this.Start && other.End == this.End;
-    //     }
+    /// <summary>
+    /// Gets whether or not the Line intersects the Rectangle.
+    /// </summary>
+    /// <param name="other">The Rectangle to check for intersection.</param>
+    /// <returns>true there is any overlap between the Line and the Rectangle.</returns>
+    public override bool Intersects(RectangleCollider other)
+    {
+        var A = new Vector2(other.shape.Left, other.shape.Top);
+        var B = new Vector2(other.shape.Right, other.shape.Top);
+        var C = new Vector2(other.shape.Right, other.shape.Bottom);
+        var D = new Vector2(other.shape.Left, other.shape.Bottom);
 
-    //     /// <summary>
-    //     /// Calculates the normalized vector pointing from point1 to point2
-    //     /// </summary>
-    //     /// <returns> A Vector2 containing the direction from point1 to point2. </returns>
-    //     public static Vector2 GetDirection(Point point1, Point point2)
-    //     {
-    //         return GetDirection(point1.ToVector2(), point2.ToVector2());
-    //     }
+        var AB = new LinePieceCollider(A, B);
+        var AD = new LinePieceCollider(A, D);
+        var BC = new LinePieceCollider(B, C);
+        var CD = new LinePieceCollider(C, D);
+
+        if (this.Intersects(AB) || this.Intersects(AD) || this.Intersects(BC) || this.Intersects(CD) || other.Contains(Start) || other.Contains(End))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Calculates the intersection point between 2 lines.
+    /// </summary>
+    /// <param name="Other">The line to intersect with</param>
+    /// <returns>A Vector2 with the point of intersection.</returns>
+    public Vector2 GetIntersection(LinePieceCollider Other)
+    {
+        float delta = this.StandardA * Other.StandardB - Other.StandardA * this.StandardB;
+        if (delta == 0)
+        {
+            return Vector2.Zero;
+        }
+        float x = (Other.StandardB * this.StandardC - this.StandardB * Other.StandardC) / delta;
+        float y = (this.StandardA * Other.StandardC - Other.StandardA * this.StandardC) / delta;
+        Vector2 result = new Vector2(x, y);
+        return result;
+    }
+
+    /// <summary>
+    /// Finds the nearest point on a line to a given vector, taking into account if the line is .
+    /// </summary>
+    /// <param name="other">The Vector you want to find the nearest point to.</param>
+    /// <returns>The nearest point on the line.</returns>
+    public Vector2 NearestPointOnLine(Vector2 other)
+    {
+        //https://stackoverflow.com/questions/3120357/get-closest-point-to-a-line
+        var AP = other - Start;
+        var AB = End - Start;
+
+        var magnitudeAB = AB.LengthSquared();
+        var ABAPproduct = Vector2.Dot(AP, AB);
+        var distance = ABAPproduct / magnitudeAB;
+        if (distance < 0)
+        {
+            return Start;
+        }
+        else if (distance > 1)
+        {
+            return End;
+        }
+        else
+        {
+            return Start + AB * distance;
+        }
+    }
+
+    /// <summary>
+    /// Returns the enclosing Axis Aligned Bounding Box containing the control points for the line.
+    /// As an unbound line has infinite length, the returned bounding box assumes the line to be bound.
+    /// </summary>
+    /// <returns></returns>
+    public override Rectangle GetBoundingBox()
+    {
+        Point topLeft = new Point((int)Math.Min(Start.X, End.X), (int)Math.Min(Start.Y, End.Y));
+        Point size = new Point((int)Math.Max(Start.X, End.X), (int)Math.Max(Start.Y, End.Y)) - topLeft;
+        return new Rectangle(topLeft, size);
+    }
 
 
-    //     /// <summary>
-    //     /// Calculates the normalized vector pointing from point1 to point2
-    //     /// </summary>
-    //     /// <returns> A Vector2 containing the direction from point1 to point2. </returns>
-    //     public Vector2 GetDirection()
-    //     {
-    //         return GetDirection(Start, End);
-    //     }
+    /// <summary>
+    /// Gets whether or not the provided coordinates lie on the line.
+    /// </summary>
+    /// <param name="coordinates">The coordinates to check.</param>
+    /// <returns>true if the coordinates are on the line.</returns>
+    public override bool Contains(Vector2 coordinates)
+    {
+        var onTheLine = (coordinates.X >= this.Start.X && coordinates.X <= this.End.X || coordinates.X >= this.End.X && coordinates.X <= this.Start.X)
+                        && (coordinates.Y >= this.Start.Y && coordinates.Y <= this.End.Y || coordinates.Y >= this.End.Y && coordinates.Y <= this.Start.Y);
+        return onTheLine;
+    }
+
+    public bool Equals(LinePieceCollider other)
+    {
+        return other.Start == this.Start && other.End == this.End;
+    }
+
+    /// <summary>
+    /// Calculates the normalized vector pointing from point1 to point2
+    /// </summary>
+    /// <returns> A Vector2 containing the direction from point1 to point2. </returns>
+    public static Vector2 GetDirection(Point point1, Point point2)
+    {
+        return GetDirection(point1.ToVector2(), point2.ToVector2());
+    }
 
 
-    //     /// <summary>
-    //     /// Should return the angle between a given direction and the up vector.
-    //     /// </summary>
-    //     /// <param name="direction">The Vector2 pointing out from (0,0) to calculate the angle to.</param>
-    //     /// <returns> The angle in radians between the the up vector and the direction to the cursor.</returns>
-    //     public float GetAngle()
-    //     {
-    //         return GetAngle(GetDirection());
-    //     }
-    // }
+    /// <summary>
+    /// Calculates the normalized vector pointing from point1 to point2
+    /// </summary>
+    /// <returns> A Vector2 containing the direction from point1 to point2. </returns>
+    public Vector2 GetDirection()
+    {
+        return GetDirection(Start, End);
+    }
+
+
+    /// <summary>
+    /// Should return the angle between a given direction and the up vector.
+    /// </summary>
+    /// <param name="direction">The Vector2 pointing out from (0,0) to calculate the angle to.</param>
+    /// <returns> The angle in radians between the the up vector and the direction to the cursor.</returns>
+    public float GetAngle()
+    {
+        return GetAngle(GetDirection());
+    }
 }
--- LastHope/Collision/RectangleCollider.cs
+++ LastHope/Collision/RectangleCollider.cs
@@ -1,133 +1,45 @@
-using System;
-using LastHope.Collision;
 using Microsoft.Xna.Framework;
+using System;
 
-namespace LastHope.Collision;
+namespace LastHope.Collision
+{
+    public class RectangleCollider : Collider, IEquatable<RectangleCollider>
+    {
+        public Rectangle shape;
 
-// public class CircleCollider : Collider, IEquatable<CircleCollider>
-// {
-//     public float X;
-//     public float Y;
-//     public Vector2 Center
-//     {
-//         get
-//         {
-//             return new Vector2(X, Y);
-//         }
+        public RectangleCollider(Rectangle shape)
+        {
+            this.shape = shape;
+        }
 
-//         set
-//         {
-//             X = value.X; Y = value.Y;
-//         }
-//     }
-//     public float Radius;
+        public override bool Contains(Vector2 loc)
+        {
+            return shape.Contains(loc);
+        }
 
-//     /// <summary>
-//     /// Creates a new Circle object.
-//     /// </summary>
-//     /// <param name="x">The X coordinate of the circle's center</param>
-//     /// <param name="y">The Y coordinate of the circle's center</param>
-//     /// <param name="radius">The radius of the circle</param>
-//     public CircleCollider(float x, float y, float radius)
-//     {
-//         this.X = x;
-//         this.Y = y;
-//         this.Radius = radius;
-//     }
+        public bool Equals(RectangleCollider other)
+        {
+            return shape == other.shape;
+        }
 
-//     /// <summary>
-//     /// Creates a new Circle object.
-//     /// </summary>
-//     /// <param name="center">The coordinates of the circle's center</param>
-//     /// <param name="radius">The radius of the circle</param>
-//     public CircleCollider(Vector2 center, float radius)
-//     {
-//         this.Center = center;
-//         this.Radius = radius;
-//     }
+        public override Rectangle GetBoundingBox()
+        {
+            return shape;
+        }
 
+        public override bool Intersects(CircleCollider other)
+        {
+            return other.Intersects(this);
+        }
 
-//     /// <summary>
-//     /// Gets whether or not the provided coordinates lie within the bounds of this Circle.
-//     /// </summary>
-//     /// <param name="coordinates">The coordinates to check.</param>
-//     /// <returns>true if the coordinates are within the circle.</returns>
-//     public override bool Contains(Vector2 coordinates)
-//     {
-//         return (Center - coordinates).Length() < Radius;
-//     }
+        public override bool Intersects(RectangleCollider other)
+        {
+            return shape.Intersects(other.shape);
+        }
 
-//     /// <summary>
-//     /// Gets whether or not the Circle intersects another Circle.
-//     /// </summary>
-//     /// <param name="other">The Circle to check for intersection.</param>
-//     /// <returns>true there is any overlap between the two Circles.</returns>
-//     public override bool Intersects(CircleCollider other)
-//     {
-//         // ||B - A|| < r1 + r2
-//         if ((Center - other.Center).Length() < Radius + other.Radius)
-//         {
-//             return true;
-//         }
-//         return false;
-//     }
-
-
-//     /// <summary>
-//     /// Gets whether or not the Circle intersects the Rectangle.
-//     /// </summary>
-//     /// <param name="other">The Rectangle to check for intersection.</param>
-//     /// <returns>true there is any overlap between the Circle and the Rectangle.</returns>
-//     public override bool Intersects(RectangleCollider other)
-//     {
-//         // if Cy < Ab & Cy > At => Cx + r > Al & Cx - r < Ar
-//         // if Cx < Ar & Cx > Al => Cy + r > At & Cy - r < Ab
-//         if (Y < other.shape.Bottom & Y > other.shape.Top)
-//         {
-//             if (X + Radius > other.shape.Left & X - Radius < other.shape.Right)
-//             {
-//                 return true;
-//             }
-//         }
-//         else if (X < other.shape.Right & X > other.shape.Left)
-//         {
-//             if (Y + Radius > other.shape.Top & Y - Radius < other.shape.Bottom)
-//             {
-//                 return true;
-//             }
-
-//         }
-
-//         Vector2 TopLeft = new Vector2(other.shape.Left, other.shape.Top);
-//         Vector2 TopRight = new Vector2(other.shape.Right, other.shape.Top);
-//         Vector2 BottomLeft = new Vector2(other.shape.Left, other.shape.Bottom);
-//         Vector2 BottomRight = new Vector2(other.shape.Right, other.shape.Bottom);
-
-//         if (this.Contains(TopLeft) || this.Contains(TopRight) || this.Contains(BottomRight) || this.Contains(BottomLeft)) return true;
-//         return false;
-//     }
-//     /// <summary>
-//     /// Gets whether or not the Circle intersects the Line
-//     /// </summary>
-//     /// <param name="other">The Line to check for intersection</param>
-//     /// <returns>true there is any overlap between the Circle and the Line.</returns>
-//     public override bool Intersects(LinePieceCollider other)
-//     {
-//         // Implemented in the line code.
-//         return other.Intersects(this);
-//     }
-
-//     /// <summary>
-//     /// Get the enclosing Rectangle that surrounds the Circle.
-//     /// </summary>
-//     /// <returns></returns>
-//     public override Rectangle GetBoundingBox()
-//     {
-//         return new Rectangle((int)(X - Radius), (int)(Y - Radius), (int)(2 * Radius), (int)(2 * Radius));
-//     }
-
-//     public bool Equals(CircleCollider other)
-//     {
-//         return other.X == X && other.Y == Y && other.Radius == Radius;
-//     }
-// }
+        public override bool Intersects(LinePieceCollider other)
+        {
+            return other.Intersects(this);
+        }
+    }
+}
