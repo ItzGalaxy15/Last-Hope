f8a2225c422526daf1f5754e903c5b1d09e17e61
Stéphane Dauvillier <dauvillier04@gmail.com>
2026-03-29 16:01:20 +0200

    Fixed arrow being slightly misaligned

--- LastHope/Classes/Weapon/Arrow.cs
+++ LastHope/Classes/Weapon/Arrow.cs
@@ -1,7 +1,6 @@
 using Last_Hope.BaseModel;
 using Last_Hope.Collision;
 using Last_Hope.Engine;
-using LastHope.Helper;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
@@ -12,34 +11,45 @@ namespace Last_Hope.Classes.Weapon
     {
         private RectangleCollider _collider;
         private Texture2D _sprite;
+        private Vector2 _position;
         private Vector2 _velocity;
-        private Point arrowSize = new Point(10, 10);
         private GameObject _owner;
         private float _damage;
         private float _critChance;
 
-
         public Arrow(Vector2 origin, Vector2 direction, float speed, GameObject owner, float damage, float critChance)
         {
             _owner = owner;
-            _collider = new RectangleCollider(new Rectangle(origin.ToPoint(), arrowSize));
-            SetCollider(_collider);
+            _position = origin;
             _velocity = direction * speed;
             _damage = damage;
             _critChance = critChance;
+            _collider = new RectangleCollider(new Rectangle(origin.ToPoint(), new Point(10, 10)));
+            SetCollider(_collider);
         }
 
         public override void Load(ContentManager content)
         {
             _sprite = content.Load<Texture2D>("Arrow");
+            _collider.shape.Size = _sprite.Bounds.Size;
             base.Load(content);
         }
 
         public override void Update(GameTime gameTime)
         {
             float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            _position += _velocity * dt;
+            _collider.shape.Location = (_position - new Vector2(_collider.shape.Width / 2f, _collider.shape.Height / 2f)).ToPoint();
             base.Update(gameTime);
-            _collider.shape.Location += (_velocity * dt).ToPoint();
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            float angle = (float)System.Math.Atan2(_velocity.Y, _velocity.X);
+            Vector2 origin = new Vector2(_sprite.Width / 2f, _sprite.Height / 2f);
+
+            spriteBatch.Draw(_sprite, _position, null, Color.White, angle, origin, 1f, SpriteEffects.None, 0f);
+            base.Draw(gameTime, spriteBatch);
         }
 
         public override void OnCollision(GameObject other)
@@ -59,6 +69,7 @@ namespace Last_Hope.Classes.Weapon
                         GameManager.GetGameManager().RemoveGameObject(this);
                     }
                     break;
+
                 case BaseEnemy:
                     if (other is BasePlayer player)
                     {
@@ -73,21 +84,11 @@ namespace Last_Hope.Classes.Weapon
             }
         }
 
-        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
-        {
-            float angle = DirectionHelper.GetAngle(_velocity);
-            Vector2 origin = new Vector2(_sprite.Width / 2f, _sprite.Height / 2f);
-            Vector2 position = _collider.shape.Center.ToVector2();
-
-            spriteBatch.Draw(_sprite, position, null, Color.White, angle, origin, 1f, SpriteEffects.None, 0f);
-            base.Draw(gameTime, spriteBatch);
-        }
-
         private int CalculateDamage()
         {
             if (GameManager.GetGameManager().RNG.NextSingle() < _critChance)
             {
-                return (int)(_damage * 1.5f);  // 1.5x damage on crit
+                return (int)(_damage * 1.5f);
             }
             return (int)_damage;
         }
