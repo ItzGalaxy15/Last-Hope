2246be15e5ab2844cf1b2c104625b476e1a48102
Amer Alhasoun <ameralhasoun@gmail.com>
2026-04-01 20:16:38 +0200

    Refactor Gamemanger

--- LastHope/Engine/GameManager.cs
+++ LastHope/Engine/GameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 
 using Last_Hope.BaseModel;
+using Last_Hope.UI;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
@@ -12,10 +13,10 @@ public class GameManager
 {
     private static GameManager gameManager;
 
-    private List<GameObject> _gameObjects;
-    private List<GameObject> _toBeRemoved;
-    private List<GameObject> _toBeAdded;
-    private ContentManager _content;
+    public List<GameObject> _gameObjects;
+    public List<GameObject> _toBeRemoved;
+    public List<GameObject> _toBeAdded;
+    public ContentManager _content;
 
     // private float _spawnTimer = 0f;
     // private float _spawnInterval = 5f;   // seconds between spawns (starts at 5 s)
@@ -35,7 +36,8 @@ public class GameManager
 
 
     public GameState _state;
-    private SpriteFont _font;
+    public SpriteFont _font;
+    public Menu Menu { get; private set; }
 
 
     public static GameManager GetGameManager()
@@ -52,6 +54,8 @@ public class GameManager
         InputManager = new InputManager();
         RNG = new Random();
         // Camera = new Camera();
+
+        Menu = new Menu();
 
         _state = GameState.StartMenu;
     }
@@ -124,16 +128,16 @@ public class GameManager
         switch (_state)
         {
             case GameState.StartMenu:
-                UpdateStartMenu(gameTime);
+                Menu.UpdateStartMenu(gameTime);
                 break;
             case GameState.Running:
-                UpdateRunning(gameTime);
+                Menu.UpdateRunningMenu(gameTime);
                 break;
             case GameState.Paused:
-                UpdatePaused(gameTime);
+                Menu.UpdatePausedMenu(gameTime);
                 break;
             case GameState.GameOver:
-                UpdateGameOver(gameTime);
+                Menu.UpdateGameOverMenu(gameTime);
                 break;
         }
     }
@@ -144,458 +148,19 @@ public class GameManager
         switch (_state)
         {
             case GameState.StartMenu:
-                DrawStartMenu(gameTime, spriteBatch);
+                Menu.DrawStartMenu(gameTime, spriteBatch);
                 break;
             case GameState.Running:
-                DrawRunning(gameTime, spriteBatch, transformMatrix);
+                Menu.DrawRunningMenu(gameTime, spriteBatch, transformMatrix);
                 break;
             case GameState.Paused:
-                DrawPaused(gameTime, spriteBatch, transformMatrix);
+                Menu.DrawPausedMenu(gameTime, spriteBatch, transformMatrix);
                 break;
             case GameState.GameOver:
-                DrawGameOver(gameTime, spriteBatch, transformMatrix);
+                Menu.DrawGameOverMenu(gameTime, spriteBatch, transformMatrix);
                 break;
         }
     }
-
-
-    private Rectangle GetTextRectangle(string text, Vector2 position, float scale = 1)
-    {
-        Vector2 size = _font.MeasureString(text) * scale;
-        // -10, -5, +20, +10 to give some padding around the text for easier clicking
-        return new Rectangle(
-            (int)position.X - 10,
-            (int)position.Y - 5,
-            (int)size.X + 20,
-            (int)size.Y + 10
-        );
-    }
-    private void UpdateStartMenu(GameTime gameTime)
-    {
-        string startText = "Start Game";
-        Vector2 startPos = GetFontPosition(startText);
-        Rectangle startRect = GetTextRectangle(startText, startPos);
-
-        string quitText = "Quit Game";
-        Vector2 quitPos = GetFontPosition(quitText) + new Vector2(0, 100);
-        Rectangle quitRect = GetTextRectangle(quitText, quitPos);
-
-
-        if (startRect.Contains(InputManager.CurrentMouseState.Position) && InputManager.LeftMousePress())
-        {
-            _state = GameState.Running;
-        }
-
-        if (quitRect.Contains(InputManager.CurrentMouseState.Position) && InputManager.LeftMousePress())
-        {
-            Game.Exit();
-        }
-    }
-
-
-    private void DrawStartMenu(GameTime gametime, SpriteBatch spriteBatch, Matrix? transformMatrix = null)
-    {
-        string startText = "Start Game";
-        Vector2 startPos = GetFontPosition(startText);
-        Rectangle startRect = GetTextRectangle(startText, startPos);
-
-
-        string quitText = "Quit Game";
-        Vector2 quitPos = GetFontPosition(quitText) + new Vector2(0, 100);
-        Rectangle quitRect = GetTextRectangle(quitText, quitPos);
-
-        spriteBatch.Begin();
-        spriteBatch.Draw(Pixel, startRect, Color.DarkSlateGray);
-        spriteBatch.Draw(Pixel, quitRect, Color.DarkSlateGray);
-        spriteBatch.DrawString(_font, startText, startPos, Color.White);
-        spriteBatch.DrawString(_font, quitText, quitPos, Color.Red);
-        spriteBatch.End();
-    }
-
-    //public void UpdateStartMenu(GameTime gameTime)
-    //{
-    //    if (InputManager.IsKeyPress(Keys.Enter)) _state = GameState.Running;
-    //    if (InputManager.IsKeyPress(Keys.Escape)) Game.Exit();
-
-    //}
-
-    //public void DrawStartMenu(GameTime gameTime, SpriteBatch spriteBatch, Matrix? transformMatrix = null)
-    //{
-    //    string enterText = "Press Enter to start the Game";
-    //    string escapeText = "Press Escape to quit";
-    //    Vector2 positionEnter = GetFontPosition(enterText);
-    //    Vector2 positionEscape = GetFontPosition(escapeText);
-
-    //    spriteBatch.Begin();
-    //    spriteBatch.DrawString(_font, enterText, positionEnter, Color.White);
-    //    spriteBatch.DrawString(_font, escapeText, positionEscape + new Vector2(0, 100), Color.Red);
-    //    spriteBatch.End();
-    //}
-
-
-    public void UpdateRunning(GameTime gameTime)
-    {
-        //Vector2 topLeft = new Vector2(20, 20);
-
-        //string pauseText = "Pause Game";
-        //Vector2 pausePos = topLeft;
-        //Rectangle pauseRect = GetTextRectangle(pauseText, pausePos);
-
-        //string quitText = "Quit Game";
-        //Vector2 quitPos = topLeft + new Vector2(600, 0);
-        //Rectangle quitRect = GetTextRectangle(quitText, quitPos);
-
-        float scale = 0.5f;
-        Vector2 topLeft = new Vector2(20, 100);
-
-        string pauseText = "Pause Game";
-        Vector2 pausePos = topLeft + new Vector2(10, 5);
-        Rectangle pauseRect = GetTextRectangle(pauseText, pausePos, scale);
-
-        Vector2 pauseSize = _font.MeasureString(pauseText) * scale;
-        string quitText = "Quit Game";
-        Vector2 quitPos = pausePos + new Vector2(pauseSize.X + 40, 0);
-        Rectangle quitRect = GetTextRectangle(quitText, quitPos, scale);
-
-        if (pauseRect.Contains(InputManager.CurrentMouseState.Position) && InputManager.LeftMousePress())
-        {
-            _state = GameState.Paused;
-            return;
-        }
-
-        if (quitRect.Contains(InputManager.CurrentMouseState.Position) && InputManager.LeftMousePress())
-        {
-            Game.Exit();
-            return;
-        }
-
-        // Handle input
-        HandleInput(InputManager);
-
-
-        // Update
-        foreach (GameObject gameObject in _gameObjects)
-        {
-            gameObject.Update(gameTime);
-        }
-
-        // Check Collission
-        CheckCollision();
-
-        foreach (GameObject gameObject in _toBeAdded)
-        {
-            gameObject.Load(_content);
-            _gameObjects.Add(gameObject);
-        }
-        _toBeAdded.Clear();
-
-        foreach (GameObject gameObject in _toBeRemoved)
-        {
-            gameObject.Destroy();
-            _gameObjects.Remove(gameObject);
-        }
-        _toBeRemoved.Clear();
-    }
-
-
-    public void DrawRunning(GameTime gameTime, SpriteBatch spriteBatch, Matrix? transformMatrix = null)
-    {
-
-        //Vector2 topLeft = new Vector2(20, 20);
-
-        //string pauseText = "Pause Game";
-        //Vector2 pausePos = topLeft;
-        //Rectangle pauseRect = GetTextRectangle(pauseText, pausePos);
-
-        //string quitText = "Quit Game";
-        //Vector2 quitPos = topLeft + new Vector2(600, 0);
-        //Rectangle quitRect = GetTextRectangle(quitText, quitPos);
-
-        float scale = 0.5f;
-        Vector2 topLeft = new Vector2(20, 100);
-
-        string pauseText = "Pause Game";
-        Vector2 pausePos = topLeft + new Vector2(10, 5);
-        Rectangle pauseRect = GetTextRectangle(pauseText, pausePos, scale);
-
-
-        Vector2 pauseSize = _font.MeasureString(pauseText) * scale;
-        string quitText = "Quit Game";
-        Vector2 quitPos = pausePos + new Vector2(pauseSize.X + 40, 0);
-        Rectangle quitRect = GetTextRectangle(quitText, quitPos, scale);
-
-        spriteBatch.Begin(transformMatrix: transformMatrix);
-        foreach (GameObject gameObject in _gameObjects)
-        {
-            gameObject.Draw(gameTime, spriteBatch);
-        }
-        spriteBatch.End();
-
-        spriteBatch.Begin();
-        spriteBatch.Draw(Pixel, pauseRect, Color.DarkSlateGray);
-        spriteBatch.Draw(Pixel, quitRect, Color.DarkSlateGray);
-        spriteBatch.DrawString(_font, pauseText, pausePos, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
-        spriteBatch.DrawString(_font, quitText, quitPos, Color.Red, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
-        //spriteBatch.DrawString(_font, pauseText, pausePos, Color.White);
-        //spriteBatch.DrawString(_font, quitText,quitPos, Color.White);
-        spriteBatch.End();
-    }
-
-    //public void UpdateRunning(GameTime gameTime)
-    //{
-
-    //    if (InputManager.IsKeyPress(Keys.Space))
-    //    {
-    //        _state = GameState.Paused;
-    //        return;
-    //    }
-    //    if (InputManager.IsKeyPress(Keys.Escape))
-    //    {
-    //        Game.Exit();
-    //        return;
-    //    }
-
-    //    // Handle input
-    //    HandleInput(InputManager);
-
-
-    //    // Update
-    //    foreach (GameObject gameObject in _gameObjects)
-    //    {
-    //        gameObject.Update(gameTime);
-    //    }
-
-
-    //    // Check Collission
-    //    CheckCollision();
-
-    //    foreach (GameObject gameObject in _toBeAdded)
-    //    {
-    //        gameObject.Load(_content);
-    //        _gameObjects.Add(gameObject);
-    //    }
-    //    _toBeAdded.Clear();
-
-    //    foreach (GameObject gameObject in _toBeRemoved)
-    //    {
-    //        gameObject.Destroy();
-    //        _gameObjects.Remove(gameObject);
-    //    }
-    //    _toBeRemoved.Clear();
-    //}
-
-
-    //public void DrawRunning(GameTime gameTime, SpriteBatch spriteBatch, Matrix? transformMatrix = null)
-    //{
-
-    //    string spaceText = "Press Space to pause the Game";
-    //    string escapeText = "Press Escape to quit";
-    //    Vector2 topLeft = new Vector2(20, 20);
-
-    //    spriteBatch.Begin(transformMatrix: transformMatrix);
-    //    foreach (GameObject gameObject in _gameObjects)
-    //    {
-    //        gameObject.Draw(gameTime, spriteBatch);
-    //    }
-    //    spriteBatch.End();
-
-    //    spriteBatch.Begin();
-    //    spriteBatch.DrawString(_font, spaceText, topLeft, Color.White);
-    //    spriteBatch.DrawString(_font, escapeText, topLeft + new Vector2(0, 100), Color.Red);
-    //    spriteBatch.End();
-    //}
-
-
-
-    public void UpdatePaused(GameTime gameTime)
-    {
-        string continueText = "Continue Game";
-        Vector2 continuePos = GetFontPosition(continueText);
-        Rectangle continueRect = GetTextRectangle(continueText, continuePos);
-
-        string quitText = "Quit Game";
-        Vector2 quitPos = GetFontPosition(quitText) + new Vector2(0, 100);
-        Rectangle quitRect = GetTextRectangle(quitText, quitPos);
-
-        if (continueRect.Contains(InputManager.CurrentMouseState.Position) && InputManager.LeftMousePress())
-        {
-            _state = GameState.Running;
-        }
-  
-        if (quitRect.Contains(InputManager.CurrentMouseState.Position) && InputManager.LeftMousePress())
-        {
-            Game.Exit();
-        }
-    }
-
-
-    public void DrawPaused(GameTime gameTime, SpriteBatch spriteBatch, Matrix? transformMatrix = null)
-    {
-
-        string continueText = "Continue Game";
-        Vector2 continuePos = GetFontPosition(continueText);
-        Rectangle continueRect = GetTextRectangle(continueText, continuePos);
-
-        string quitText = "Quit Game";
-        Vector2 quitPos = GetFontPosition(quitText) + new Vector2(0, 100);
-        Rectangle quitRect = GetTextRectangle(quitText, quitPos);
-
-        spriteBatch.Begin(transformMatrix: transformMatrix);
-        foreach (GameObject gameObject in _gameObjects)
-        {
-            gameObject.Draw(gameTime, spriteBatch);
-        }
-        spriteBatch.End();
-
-        spriteBatch.Begin();
-        spriteBatch.Draw(Pixel, continueRect, Color.DarkSlateGray);
-        spriteBatch.Draw(Pixel, quitRect, Color.DarkSlateGray);
-        spriteBatch.DrawString(_font, continueText, continuePos, Color.White);
-        spriteBatch.DrawString(_font, quitText, quitPos, Color.Red);
-        spriteBatch.End();
-
-    }
-
-
-    //public void UpdatePaused(GameTime gameTime)
-    //{
-    //    if (InputManager.IsKeyPress(Keys.Space)) _state = GameState.Running;
-    //    if (InputManager.IsKeyPress(Keys.Escape)) Game.Exit();
-
-    //}
-
-
-    //public void DrawPaused(GameTime gameTime, SpriteBatch spriteBatch, Matrix? transformMatrix = null)
-    //{
-    //    string spaceText = "Press Space to continue the Game";
-    //    string escapeText = "Press Escape to quit";
-    //    Vector2 positionEnter = GetFontPosition(spaceText);
-    //    Vector2 positionEscape = GetFontPosition(escapeText);
-
-
-    //    spriteBatch.Begin(transformMatrix: transformMatrix);
-    //    foreach (GameObject gameObject in _gameObjects)
-    //    {
-    //        gameObject.Draw(gameTime, spriteBatch);
-    //    }
-    //    spriteBatch.End();
-
-    //    spriteBatch.Begin();
-    //    spriteBatch.DrawString(_font, spaceText, positionEnter, Color.White);
-    //    spriteBatch.DrawString(_font, escapeText, positionEscape + new Vector2(0, 100), Color.Red);
-    //    spriteBatch.End();
-    //}
-
-
-    public void UpdateGameOver(GameTime gameTime)
-    {
-        string restartText = "Restart Game";
-        Vector2 restartPos = GetFontPosition(restartText) + new Vector2(0, 100);
-        Rectangle restartRect = GetTextRectangle(restartText, restartPos);
-
-        string quitText = "Quit Game";
-        Vector2 quitPos = GetFontPosition(quitText) + new Vector2(0, 200);
-        Rectangle quitRect = GetTextRectangle(quitText, quitPos);
-
-        if (restartRect.Contains(InputManager.CurrentMouseState.Position) && InputManager.LeftMousePress())
-        {
-            ResetGame();
-            _state = GameState.Running;
-        }
-
-        if (quitRect.Contains(InputManager.CurrentMouseState.Position) && InputManager.LeftMousePress())
-        {
-            Game.Exit();
-        }
-    }
-
-
-
-    public void DrawGameOver(GameTime gameTime, SpriteBatch spriteBatch, Matrix? transformMatrix = null)
-    {
-        string gameOverText = "Game Over";
-        Vector2 positrionGameOver = GetFontPosition(gameOverText);
-
-        string restartText = "Restart Game";
-        Vector2 restartPos = GetFontPosition(restartText) + new Vector2(0, 100);
-        Rectangle restartRect = GetTextRectangle(restartText, restartPos);
-
-        string quitText = "Quit Game";
-        Vector2 quitPos = GetFontPosition(quitText) + new Vector2(0, 200);
-        Rectangle quitRect = GetTextRectangle(quitText, quitPos);
-
-
-        spriteBatch.Begin(transformMatrix: transformMatrix);
-        foreach (GameObject gameObject in _gameObjects)
-        {
-            gameObject.Draw(gameTime, spriteBatch);
-        }
-
-        spriteBatch.End();
-
-        spriteBatch.Begin();
-        spriteBatch.DrawString(_font, gameOverText, positrionGameOver, Color.Red);
-        spriteBatch.Draw(Pixel, restartRect, Color.DarkSlateGray);
-        spriteBatch.Draw(Pixel, quitRect, Color.DarkSlateGray);
-        spriteBatch.DrawString(_font, restartText, restartPos, Color.White);
-        spriteBatch.DrawString(_font, quitText, quitPos, Color.Red);
-        spriteBatch.End();
-    }
-
-    //public void UpdateGameOver(GameTime gameTime)
-    //{
-    //    if (InputManager.IsKeyPress(Keys.Enter))
-    //    {
-    //        ResetGame();
-    //        _state = GameState.Running;
-    //    }
-
-
-    //    if (InputManager.IsKeyPress(Keys.Escape)) Game.Exit();
-    //}
-
-
-    //public void DrawGameOver(GameTime gameTime, SpriteBatch spriteBatch, Matrix? transformMatrix = null)
-    //{
-    //    string gameOverText = "Game Over";
-    //    string enterText = "Press Enter to restart the Game";
-    //    string escapeText = "Press Escape to quit";
-    //    Vector2 positrionGameOver = GetFontPosition(gameOverText);
-    //    Vector2 positionEnter = GetFontPosition(enterText);
-    //    Vector2 positionEscape = GetFontPosition(escapeText);
-
-    //    spriteBatch.Begin(transformMatrix: transformMatrix);
-    //    foreach (GameObject gameObject in _gameObjects)
-    //    {
-    //        gameObject.Draw(gameTime, spriteBatch);
-    //    }
-
-    //    spriteBatch.End();
-
-    //    spriteBatch.Begin();
-    //    spriteBatch.DrawString(_font, gameOverText, positrionGameOver, Color.Red);
-    //    spriteBatch.DrawString(_font, enterText, positionEnter + new Vector2(0, 100), Color.White);
-    //    spriteBatch.DrawString(_font, escapeText, positionEscape + new Vector2(0, 200), Color.Red);
-    //    spriteBatch.End();
-    //}
-
-
-    public Vector2 GetFontPosition(string text)
-    {
-        Viewport viewport = Game.GraphicsDevice.Viewport;
-        Vector2 center = new Vector2(viewport.Width / 2f, viewport.Height / 2f);
-        if (_font == null)
-        {
-            // Safe fallback until content is loaded.
-            return center;
-        }
-
-        Vector2 textSize = _font.MeasureString(text);
-        Vector2 position = new Vector2(center.X - textSize.X / 2f, center.Y - textSize.Y / 2f);
-        return position;
-    }
-
-
 
 
     /// <summary>
