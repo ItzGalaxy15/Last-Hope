2d48ec4a3b5de7b8aad23ddd33d76baa8a317959
Amer Alhasoun <ameralhasoun@gmail.com>
2026-04-20 17:40:12 +0200

    Remove Quit and restart buttons from the running menu and place them in pause menu

--- LastHope/UI/Menus/PausedMenu.cs
+++ LastHope/UI/Menus/PausedMenu.cs
@@ -10,12 +10,22 @@ public class PausedMenu : MenuBase
     public void Update(GameTime gameTime)
     {
 
+        string restartText = "Restart Game";
+        Vector2 restartPos = GetFontPosition(restartText);
+        Rectangle restartRect = GetTextRectangle(restartText, restartPos);
+
         string quitText = "Quit Game";
-        Vector2 quitPos = GetFontPosition(quitText);
+        Vector2 quitPos = GetFontPosition(quitText) + new Vector2(0, 100);
         Rectangle quitRect = GetTextRectangle(quitText, quitPos);
 
         if (InputManager.IsKeyPress(Keys.Escape))
         {
+            _state = GameState.Running;
+        }
+
+        if (restartRect.Contains(InputManager.CurrentMouseState.Position) && InputManager.LeftMousePress())
+        {
+            gm.ResetGame();
             _state = GameState.Running;
         }
 
@@ -27,15 +37,21 @@ public class PausedMenu : MenuBase
 
     public void Draw(GameTime gameTime, SpriteBatch spriteBatch, Matrix? transformMatrix = null)
     {
+        string restartText = "Restart Game";
+        Vector2 restartPos = GetFontPosition(restartText);
+        Rectangle restartRect = GetTextRectangle(restartText, restartPos);
+
         string quitText = "Quit Game";
-        Vector2 quitPos = GetFontPosition(quitText);
+        Vector2 quitPos = GetFontPosition(quitText) + new Vector2(0, 100);
         Rectangle quitRect = GetTextRectangle(quitText, quitPos);
 
         DrawWorld(gameTime, spriteBatch, transformMatrix);
 
         spriteBatch.Begin();
         DrawControlsText(spriteBatch, gameTime);
+        spriteBatch.Draw(Pixel, restartRect, Color.DarkSlateGray);
         spriteBatch.Draw(Pixel, quitRect, Color.DarkSlateGray);
+        spriteBatch.DrawString(_font, restartText, restartPos, Color.White);
         spriteBatch.DrawString(_font, quitText, quitPos, Color.Red);
         spriteBatch.End();
     }
--- LastHope/UI/Menus/RunningMenu.cs
+++ LastHope/UI/Menus/RunningMenu.cs
@@ -10,34 +10,11 @@ public class RunningMenu : MenuBase
 {
     public void Update(GameTime gameTime)
     {
-        float scale = 0.5f;
-        Vector2 topLeft = new Vector2(20, 100);
-
-        string quitText = "Quit Game";
-        Vector2 quitPos = topLeft + new Vector2(10, 5);
-        Rectangle quitRect = GetTextRectangle(quitText, quitPos, scale);
-
-        Vector2 quitSize = _font.MeasureString(quitText) * scale;
-        string restartText = "Restart Game";
-        Vector2 restartPos = quitPos + new Vector2(quitSize.X + 40, 0);
-        Rectangle restartRect = GetTextRectangle(restartText, restartPos, scale);
 
         if (InputManager.IsKeyPress(Keys.Escape))
         {
             _state = GameState.Paused;
             return;
-        }
-
-        if (quitRect.Contains(InputManager.CurrentMouseState.Position) && InputManager.LeftMousePress())
-        {
-            Game.Exit();
-            return;
-        }
-
-        if (restartRect.Contains(InputManager.CurrentMouseState.Position) && InputManager.LeftMousePress())
-        {
-            gm.ResetGame();
-            _state = GameState.Running;
         }
 
         gm.HandleInput(InputManager);
@@ -66,25 +43,6 @@ public class RunningMenu : MenuBase
 
     public void Draw(GameTime gameTime, SpriteBatch spriteBatch, Matrix? transformMatrix = null)
     {
-        float scale = 0.5f;
-        Vector2 topLeft = new Vector2(20, 100);
-
-        string quitText = "Quit Game";
-        Vector2 quitPos = topLeft + new Vector2(10, 5);
-        Rectangle quitRect = GetTextRectangle(quitText, quitPos, scale);
-
-        Vector2 quitSize = _font.MeasureString(quitText) * scale;
-        string restartText = "Restart Game";
-        Vector2 restartPos = quitPos + new Vector2(quitSize.X + 40, 0);
-        Rectangle restartRect = GetTextRectangle(restartText, restartPos, scale);
-
         DrawWorld(gameTime, spriteBatch, transformMatrix);
-
-        spriteBatch.Begin();
-        spriteBatch.Draw(Pixel, quitRect, Color.DarkSlateGray);
-        spriteBatch.Draw(Pixel, restartRect, Color.DarkSlateGray);
-        spriteBatch.DrawString(_font, quitText, quitPos, Color.Red, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
-        spriteBatch.DrawString(_font, restartText, restartPos, Color.Green, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
-        spriteBatch.End();
     }
 }
