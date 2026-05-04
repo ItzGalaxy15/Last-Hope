c53e6cf804cc95ca0bd03df3320b9c832a0ee40d
Vendux1 <declanversloot@gmail.com>
2026-05-05 01:16:36 +0200

    refactor: Reorganize GameObject class structure for improved readability and maintainability

--- LastHope/Engine/GameObject.cs
+++ LastHope/Engine/GameObject.cs
@@ -5,109 +5,110 @@ using Microsoft.Xna.Framework.Graphics;
 
 
 namespace Last_Hope.Engine;
-    public abstract class GameObject
+
+public abstract class GameObject
+{
+
+    public Collider GetCollider() => collider;
+
+    protected Collider collider;
+
+    protected const float HurtFlashDurationSeconds = 0.12f;
+    protected static readonly Color HurtFlashColor = Color.Lerp(Color.White, Color.Red, 0.35f);
+    private float _hurtFlashTimer;
+    protected Color DrawTint => _hurtFlashTimer > 0f ? HurtFlashColor : Color.White;
+
+    /// <summary>
+    /// Starts the hurt flash tint. Call this from Damage implementations
+    /// whenever HP is removed so subclasses can render <see cref="DrawTint"/>.
+    /// </summary>
+    protected void TriggerHurtFlash()
+    {
+        _hurtFlashTimer = HurtFlashDurationSeconds;
+    }
+
+    /// <summary>
+    /// Used to set the collider used for object collision.
+    /// </summary>
+    /// <param name="collider"> The collider to be used. </param>
+    public void SetCollider(Collider collider)
+    {
+        this.collider = collider;
+    }
+
+    /// <summary>
+    /// Override this method to load in assets for your class.
+    /// </summary>
+    /// <param name="content"> The ContentManager that handles file loading. </param>
+    /// <example>
+    /// To load a texture use: 
+    /// content.Load<Texture2D>([texture name]);
+    /// </example>
+    public virtual void Load(ContentManager content)
     {
 
-        public Collider GetCollider() => collider;
+    }
 
-        protected Collider collider;
+    /// <summary>
+    /// Override this if you want your GameObject to react to user input.
+    /// </summary>
+    /// <param name="inputManager"> Keeps track of user input. </param>
+    public virtual void HandleInput(InputManager inputManager)
+    {
 
-        protected const float HurtFlashDurationSeconds = 0.12f;
-        protected static readonly Color HurtFlashColor = Color.Lerp(Color.White, Color.Red, 0.35f);
-        private float _hurtFlashTimer;
-        protected Color DrawTint => _hurtFlashTimer > 0f ? HurtFlashColor : Color.White;
+    }
 
-        /// <summary>
-        /// Starts the hurt flash tint. Call this from Damage implementations
-        /// whenever HP is removed so subclasses can render <see cref="DrawTint"/>.
-        /// </summary>
-        protected void TriggerHurtFlash()
+    /// <summary>
+    /// Checks if this GameObject collides with the other GameObject
+    /// </summary>
+    /// <param name="other"> The GameObject to check collision with. </param>
+    /// <returns></returns>
+    public bool CheckCollision(GameObject other)
+    {
+        if (other == null || collider == null || other.collider == null)
+            return false;
+        return collider.CheckIntersection(other.collider);
+    }
+
+    /// <summary>
+    /// Override this if you want the GameObject to respond to collision events.
+    /// Keep in mind that OnCollision will be called once on both Objects.
+    /// </summary>
+    /// <param name="other"> The GameObject with which it collided. </param>
+    public virtual void OnCollision(GameObject other)
+    {
+
+    }
+
+    /// <summary>
+    /// Called every game step. Override this to keep your GameObject up to date.
+    /// </summary>
+    /// <param name="gameTime"> The amount of time that has elapsed since the last update call. </param>
+    public virtual void Update(GameTime gameTime)
+    {
+        if (_hurtFlashTimer > 0f)
         {
-            _hurtFlashTimer = HurtFlashDurationSeconds;
-        }
-
-        /// <summary>
-        /// Used to set the collider used for object collision.
-        /// </summary>
-        /// <param name="collider"> The collider to be used. </param>
-        public void SetCollider(Collider collider)
-        {
-            this.collider = collider;
-        }
-
-        /// <summary>
-        /// Override this method to load in assets for your class.
-        /// </summary>
-        /// <param name="content"> The ContentManager that handles file loading. </param>
-        /// <example>
-        /// To load a texture use: 
-        /// content.Load<Texture2D>([texture name]);
-        /// </example>
-        public virtual void Load(ContentManager content)
-        {
-
-        }
-
-        /// <summary>
-        /// Override this if you want your GameObject to react to user input.
-        /// </summary>
-        /// <param name="inputManager"> Keeps track of user input. </param>
-        public virtual void HandleInput(InputManager inputManager)
-        {
-
-        }
-
-        /// <summary>
-        /// Checks if this GameObject collides with the other GameObject
-        /// </summary>
-        /// <param name="other"> The GameObject to check collision with. </param>
-        /// <returns></returns>
-        public bool CheckCollision(GameObject other)
-        {
-            if (collider == null || other == null || other.collider == null)
-                return false;
-            return collider.CheckIntersection(other.collider);
-        }
-
-        /// <summary>
-        /// Override this if you want the GameObject to respond to collision events.
-        /// Keep in mind that OnCollision will be called once on both Objects.
-        /// </summary>
-        /// <param name="other"> The GameObject with which it collided. </param>
-        public virtual void OnCollision(GameObject other)
-        {
-
-        }
-
-        /// <summary>
-        /// Called every game step. Override this to keep your GameObject up to date.
-        /// </summary>
-        /// <param name="gameTime"> The amount of time that has elapsed since the last update call. </param>
-        public virtual void Update(GameTime gameTime)
-        {
-            if (_hurtFlashTimer > 0f)
-            {
-                _hurtFlashTimer -= (float)gameTime.ElapsedGameTime.TotalSeconds;
-                if (_hurtFlashTimer < 0f)
-                    _hurtFlashTimer = 0f;
-            }
-        }
-        /// <summary>
-        /// Called every game step. Override this with any drawing code you wish to implement.
-        /// SpriteBatch.Begin() and SpriteBatch.End() are called in the GameManager, so you should not call them for every draw call.
-        /// </summary>
-        /// <param name="gameTime"> The amount of time that has elapsed since the last draw call. </param>
-        /// <param name="spriteBatch"> The Spritebatch to write your textures to. </param>
-        public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch) 
-        {
-
-        }
-
-        /// <summary>
-        /// Called when the GameObject is removed from the GameManager.
-        /// </summary>
-        public virtual void Destroy() 
-        {
-
+            _hurtFlashTimer -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (_hurtFlashTimer < 0f)
+                _hurtFlashTimer = 0f;
         }
     }
+    /// <summary>
+    /// Called every game step. Override this with any drawing code you wish to implement.
+    /// SpriteBatch.Begin() and SpriteBatch.End() are called in the GameManager, so you should not call them for every draw call.
+    /// </summary>
+    /// <param name="gameTime"> The amount of time that has elapsed since the last draw call. </param>
+    /// <param name="spriteBatch"> The Spritebatch to write your textures to. </param>
+    public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch) 
+    {
+
+    }
+
+    /// <summary>
+    /// Called when the GameObject is removed from the GameManager.
+    /// </summary>
+    public virtual void Destroy() 
+    {
+
+    }
+}
